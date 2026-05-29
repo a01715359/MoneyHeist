e92afb513bd69b85313f2165aaebb9e55a409af0
a01715359 <a01715359@tec.mx>
2026-05-29 08:39:51 -0600

    Update Gadget.h

--- Gadget.h
+++ Gadget.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "Tool.h"
 
@@ -16,10 +17,22 @@ class Gadget: public Tool{
         Gadget(std::string n, int useTool, float suspicion, bool elec)
             : Tool(n, useTool, suspicion), isElectronic(elec) {}
         bool getIsElectronic();
+        std::string use() override;
 };
 
 bool Gadget::getIsElectronic(){
     return isElectronic;
+}
+
+std::string Gadget::use(){
+    consumeUse(1);
+    std::stringstream ss;
+    ss << uses;
+    if (isElectronic) {
+        return name + ": incapacitado instantaneamente. Usos restantes: " + ss.str();
+    } else {
+        return name + ": sin rastro, sin testigos. Usos restantes: " + ss.str();
+    }
 }
 
 #endif
