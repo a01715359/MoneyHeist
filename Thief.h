a3029c164a5f7ca98020f345d401c630746a2716
a01715359 <a01715359@tec.mx>
2026-06-12 08:57:58 -0600

    Refactor Thief class and update method signatures

--- Thief.h
+++ Thief.h
@@ -1,4 +1,3 @@
-
 #ifndef THIEF_H
 #define THIEF_H
 
@@ -6,47 +5,60 @@
 #include <string>
 #include <vector>
 
-#include "Weapon.h"
 #include "Gadget.h"
+#include "Weapon.h"
 
-class Thief{
-    private:
-        std::string name;
-        int hp;
-        float suspicion;
-        bool goldenKey;
-        std::vector<Tool*> inventory;
+class Thief {
+private:
+    std::string name;
+    int hp;
+    float suspicion;
+    bool goldenKey;
+    std::vector<Tool*> inventory;
 
-    public:
-        Thief()
-            : name(""), hp(0), suspicion(0.0), goldenKey(false){};
+public:
+    Thief()
+        : name(""), hp(0), suspicion(0.0), goldenKey(false) {}
 
-        Thief(std::string nThief, int health, float susThief, bool key)
-            : name(nThief), hp(health), suspicion(susThief), goldenKey(key) {}
-        
-        std::string getName();
-        int getHp();
-        float getSuspicion();
-        bool getGoldenKey();
+    Thief(std::string nThief, int hpThief,
+          float susThief, bool key)
+        : name(nThief),
+          hp(hpThief),
+          suspicion(susThief),
+          goldenKey(key) {}
 
-        std::vector<Tool*> getInventory();
+    std::string getName();
+    int getHp();
+    float getSuspicion();
+    bool getGoldenKey();
 
-        void setGoldenKey(bool hasKey);
-        void setHp(int health);
-        void addWeapon(std::string nombre, int usos, float sospecha, bool extra1, bool extra2);
-        void addGadget(std::string nombre, int usos, float sospecha, bool extra1);
-        // void addToInventory(int tipo, std::string nombre, int usos, float sospecha, bool extra1, bool extra2);
-        void removeFromInventory(int index);
-        void addSuspicion(float amountSuspicion);
-        void takeDamage(int amountDamage);
-        bool isAlive();
+    std::vector<Tool*>& getInventory();
+
+    void setGoldenKey(bool hasKey);
+    void setHp(int damage);
+
+    void addWeapon(std::string name,
+                   int uses,
+                   float suspicion,
+                   bool extra1);
+
+    void addGadget(std::string name,
+                   int uses,
+                   float suspicion,
+                   bool extra1);
+
+    void removeFromInventory(int index);
+
+    void addSuspicion(float amountSuspicion);
+
+    bool isAlive();
 };
 
-std::string Thief::getName(){
+std::string Thief::getName() {
     return name;
 }
 
-int Thief::getHp(){
+int Thief::getHp() {
     return hp;
 }
 
@@ -58,66 +70,64 @@ bool Thief::getGoldenKey() {
     return goldenKey;
 }
 
-std::vector<Tool*> Thief::getInventory(){
+std::vector<Tool*>& Thief::getInventory() {
     return inventory;
 }
 
-void Thief::setGoldenKey(bool hasKey){
+void Thief::setGoldenKey(bool hasKey) {
     goldenKey = hasKey;
 }
 
-void Thief::setHp(int health){
-    hp = health;
+void Thief::addWeapon(std::string name,
+                      int uses,
+                      float suspicion,
+                      bool extra1) {
+
+    if (inventory.size() < 2) {
+
+        Tool* newTool = nullptr;
+
+        newTool = new Weapon(name,
+                             uses,
+                             suspicion,
+                             extra1);
+
+        inventory.push_back(newTool);
+    }
 }
 
-void Thief::addWeapon(std::string nombre, int usos, float sospecha, bool extra1, bool extra2) {
-    if(inventory.size() < 2){
-    
-        Tool* nueva = nullptr;
-        nueva = new Weapon(nombre, usos, sospecha, extra1, extra2);
-        inventory.push_back(nueva);
-    }
-}
+void Thief::addGadget(std::string name,
+                      int uses,
+                      float suspicion,
+                      bool extra1) {
 
-void Thief::addGadget(std::string nombre, int usos, float sospecha, bool extra1) {
-    if(inventory.size() < 2){
-    
-        Tool* nueva = nullptr;
-        nueva = new Gadget(nombre, usos, sospecha, extra1);
-        inventory.push_back(nueva);
+    if (inventory.size() < 2) {
+
+        Tool* newTool = nullptr;
+
+        newTool = new Gadget(name,
+                             uses,
+                             suspicion,
+                             extra1);
+
+        inventory.push_back(newTool);
     }
 }
-/*
-void Thief::addToInventory(int tipo, std::string nombre, int usos, float sospecha, bool extra1, bool extra2) {
-    if(inventory.size() < 2){
-    
-        Tool* nueva = nullptr;
-        
-        if(tipo == 1 || tipo == 2){
-            nueva = new Weapon(nombre, usos, sospecha, extra1, extra2);
-        }
-        else{
-            nueva = new Gadget(nombre, usos, sospecha, extra1);
-        }
-            inventory.push_back(nueva);
-    }
-}
-*/
 
 void Thief::removeFromInventory(int index) {
     delete inventory[index];
     inventory.erase(inventory.begin() + index);
 }
 
-void Thief::addSuspicion(float amountSuspicion){
+void Thief::addSuspicion(float amountSuspicion) {
     suspicion += amountSuspicion;
 }
 
-void Thief::takeDamage(int amountDamage){
-    hp -= amountDamage;
+void Thief::setHp(int damage) {
+    hp -= damage;
 }
 
-bool Thief::isAlive(){
+bool Thief::isAlive() {
     return hp > 0;
 }
 
