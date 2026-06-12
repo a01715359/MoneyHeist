382a575ebea8ffaa2d1344ab51f8a3fe65346a68
a01715359 <a01715359@tec.mx>
2026-06-12 08:25:44 -0600

    Refactor Weapon class and update methods

--- Weapon.h
+++ Weapon.h
@@ -1,44 +1,45 @@
 #ifndef WEAPON_H
 #define WEAPON_H
 
-#include<iostream>
-#include<string>
-#include <sstream>
-
 #include "Tool.h"
+#include <string>
 
-class Weapon: public Tool{
-    private:
-        bool isRanged;
-        bool canForceLocks;
-    public:
-        Weapon()
-            : Tool(), isRanged(false), canForceLocks(false){};
-        Weapon(std::string n, int useTool, float suspicion, bool r, bool lock)
-            : Tool(n, useTool, suspicion), isRanged(r), canForceLocks(lock) {}
-        
-        bool getIsRanged();
-        bool getCanForceLocks();
-        std::string use();
+class Weapon : public Tool {
+private:
+    bool canForceLocks;
+
+public:
+    Weapon()
+        : Tool(), canForceLocks(false) {}
+
+    Weapon(const std::string& weaponName,
+           int totalUses,
+           float suspicion,
+           bool forceLocks)
+        : Tool(weaponName, totalUses, suspicion),
+          canForceLocks(forceLocks) {}
+
+    bool getCanForceLocks() const;
+
+    std::string use() override;
 };
 
-bool Weapon::getIsRanged(){
-    return isRanged;
-}
-
-bool Weapon::getCanForceLocks(){
+bool Weapon::getCanForceLocks() const {
     return canForceLocks;
 }
 
-std::string Weapon::use(){
-    consumeUse(1);
-    std::stringstream ss;
-    ss << uses;
+std::string Weapon::use() {
+    consumeUse();
+
     if (canForceLocks) {
-        return name + ": forzaste la cerradura. Usos restantes: " + ss.str();
-    } else {
-        return name + ": neutralizaste al guardia. Usos restantes: " + ss.str();
+        return getName() +
+               ": forced the lock. Remaining uses: "
+               + std::to_string(getUses());
     }
+
+    return getName() +
+           ": neutralized the guard. Remaining uses: "
+           + std::to_string(getUses());
 }
 
 #endif
